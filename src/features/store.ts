570afd6beb52e25ebe35e8d60181ea453ee37d56
teenage1ove <kirillstars7@gmail.com>
2024-07-16 11:39:47 +0300

    Add persist reducer

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,20 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import categoriesSlice from './categories/categoriesSlice'
 import productsSlice from './products/productsSlice'
 import userSlice from './user/userSlice'
 // ...
 
+const rootReducer = combineReducers({
+	categories: categoriesSlice,
+	products: productsSlice,
+	user: userSlice,
+	[apiSlice.reducerPath]: apiSlice.reducer,
+})
+
+const persistConfig = {
+	key: 'root',
+	storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-	reducer: {
-		categories: categoriesSlice,
-		products: productsSlice,
-		user: userSlice,
-		[apiSlice.reducerPath]: apiSlice.reducer,
-	},
+	reducer: persistedReducer,
 	middleware: getMiddleWare => getMiddleWare().concat(apiSlice.middleware),
 	devTools: true,
 })
+
+export const persister = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
